af88d850cd9e4876e536a55c2f9c678ee1e23cb3
Mrinalini Raghavendran <mrin19995@gmail.com>
2025-09-01 18:33:50 -0700

    added-new-changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Mrinalini | Portfolio`,
+    description: `Personal portfolio site built with Gatsby`,
+    author: `@mrinalini`,
+    siteUrl: `https://mrinalini-raghavendran19.github.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -28,15 +19,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mrinalini-portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
   ],
